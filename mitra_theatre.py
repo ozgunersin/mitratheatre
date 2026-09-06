7d9621b4a188c443650b61c86ab44dbca54bd30c
ozgunersin <ozgunersin@gmail.com>
2026-09-06 12:21:29 +0300

    Update mitra_theatre.py

--- mitra_theatre.py
+++ mitra_theatre.py
@@ -9,7 +9,7 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QWidget, QVBoxLayout,
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput, QMediaDevices, QVideoFrame
 from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtCore import QUrl, Qt, Qtimes
-from PySide6.QtGui import QPixmap, QIcon, QDesktopServices
+from PySide6.QtGui import QPixmap, QIcon, QDesktopServices, QColor
 
 # --- RESOURCE PATH HELPER (FOR PYINSTALLER ICON & DATA BUNDLING) ---
 def resource_path(relative_path):
